1e6561f457714f4781005cc5719bea66e9eedc2c
itzAamir <amirpc190320@gmail.com>
2022-07-20 18:52:10 +0530

    added modes

--- src/Screens/Controller.jsx
+++ src/Screens/Controller.jsx
@@ -1,10 +1,12 @@
 import GlobalStyles from "../GlobalStyles";
-import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity, StyleSheet, Text, View, Button } from "react-native";
 import React, { useEffect, useRef, useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import LottieView from "lottie-react-native";
 import axios from "axios";
 import Keypad from "../Components/Keypad";
+
+const baseUrl = "https://itzaamir.in";
 
 const Controller = ({ route }) => {
 	const { channel } = route.params;
@@ -22,7 +24,7 @@ const Controller = ({ route }) => {
 		const source = axios.CancelToken.source();
 		const interval = setInterval(() => {
 			axios
-				.get("http://192.168.4.1", { timeout: 2000, cancelToken: source.token })
+				.get(baseUrl, { timeout: 2000, cancelToken: source.token })
 				.then(() => {
 					if (animationRef.current) {
 						animationRef.current.reset();
@@ -58,10 +60,10 @@ const Controller = ({ route }) => {
 		if (channelNum === 1) {
 			const status = output1Status === "on" ? "off" : "on";
 			setOutput1Status(status);
-			axios.get("http://192.168.4.1/LED_1/" + status);
+			axios.get(baseUrl + "/LED_1/" + status);
 		} else {
 			const status = output2Status === "on" ? "off" : "on";
-			axios.get("http://192.168.4.1/LED_2/" + status);
+			axios.get(baseUrl + "/LED_2/" + status);
 			setOutput2Status(status);
 		}
 	};
@@ -72,125 +74,128 @@ const Controller = ({ route }) => {
 				colors={["#395278e3", "#263956"]}
 				style={GlobalStyles.background}
 			/>
-			<View style={{ marginTop: 20 }}>
-				{!connectionStatus.connected ? (
-					<LottieView
-						ref={animationRef}
-						source={require("../../assets/wifi-connecting.json")}
-						style={{
-							height: 200,
-							width: 200,
-							alignSelf: "center",
-						}}
-						loop
-						autoPlay
-					/>
-				) : (
-					<LottieView
-						source={require("../../assets/wifi-connected.json")}
-						style={{
-							height: 200,
-							width: 200,
-							alignSelf: "center",
-						}}
-					/>
-				)}
-
-				<View style={styles.statusWrapper}>
-					<Text style={styles.status}>Status: </Text>
-					<Text
-						style={{
-							color: connectionStatus.connecting
-								? "lightgray"
-								: connectionStatus.connected
-								? "#4DDA74"
-								: "orange",
-						}}
-					>
-						{connectionStatus.connecting
-							? "Establishing connection..."
-							: connectionStatus.connected
-							? "Connected"
-							: "Not Connected"}
-					</Text>
-				</View>
-
-				{!isStarted && (
-					<TouchableOpacity
-						activeOpacity={0.5}
-						onPress={handleGetStarted}
-						style={{
-							...styles.connectBtn,
-							backgroundColor: connectionStatus.connected
-								? "#3490dc"
-								: "darkgray",
-						}}
-						disabled={!connectionStatus.connected}
-					>
-						<Text style={{ color: "white", alignSelf: "center" }}>
-							Get Started
-						</Text>
-					</TouchableOpacity>
-				)}
-			</View>
-
-			{connectionStatus.connected && isStarted ? (
-				<View style={styles.outputContainer}>
-					<TouchableOpacity
-						style={styles.outputWrapper}
-						onPress={() => handleOutputChange(1)}
-						activeOpacity={0.8}
-					>
-						<Text style={styles.output}>Output 1</Text>
-						<View
+			<View style={{ flex: 1 }}>
+				<View style={{ marginTop: 20 }}>
+					{!connectionStatus.connected ? (
+						<LottieView
+							ref={animationRef}
+							source={require("../../assets/wifi-connecting.json")}
 							style={{
-								...styles.led,
-								backgroundColor:
-									output1Status === "on" ? "#46cf76" : "lightgray",
+								height: 200,
+								width: 200,
+								alignSelf: "center",
+							}}
+							loop
+							autoPlay
+						/>
+					) : (
+						<LottieView
+							source={require("../../assets/wifi-connected.json")}
+							style={{
+								height: 200,
+								width: 200,
+								alignSelf: "center",
 							}}
 						/>
-					</TouchableOpacity>
-					{channel === 2 && (
+					)}
+
+					<View style={styles.statusWrapper}>
+						<Text style={styles.status}>Status: </Text>
+						<Text
+							style={{
+								color: connectionStatus.connecting
+									? "lightgray"
+									: connectionStatus.connected
+									? "#4DDA74"
+									: "orange",
+							}}
+						>
+							{connectionStatus.connecting
+								? "Establishing connection..."
+								: connectionStatus.connected
+								? "Connected"
+								: "Not Connected"}
+						</Text>
+					</View>
+
+					{!isStarted && (
+						<TouchableOpacity
+							activeOpacity={0.5}
+							onPress={handleGetStarted}
+							style={{
+								...styles.connectBtn,
+								backgroundColor: connectionStatus.connected
+									? "#3490dc"
+									: "darkgray",
+							}}
+							disabled={!connectionStatus.connected}
+						>
+							<Text style={{ color: "white", alignSelf: "center" }}>
+								Get Started
+							</Text>
+						</TouchableOpacity>
+					)}
+				</View>
+
+				{connectionStatus.connected && isStarted ? (
+					<View style={styles.outputContainer}>
 						<TouchableOpacity
 							style={styles.outputWrapper}
-							onPress={() => handleOutputChange(2)}
+							onPress={() => handleOutputChange(1)}
 							activeOpacity={0.8}
 						>
-							<Text style={styles.output}>Output 2</Text>
+							<Text style={styles.output}>Output 1</Text>
 							<View
 								style={{
 									...styles.led,
 									backgroundColor:
-										output2Status === "on" ? "#46cf76" : "lightgray",
+										output1Status === "on" ? "#46cf76" : "lightgray",
 								}}
 							/>
 						</TouchableOpacity>
-					)}
-				</View>
-			) : (
-				<>
-					<Text
-						style={{
-							...styles.connectionText,
-							color: connectionStatus.failed ? "orange" : "white",
-						}}
-					>
-						{connectionStatus.failed &&
-							"Please connect your wifi with WiFi Clip"}
-					</Text>
-					<Text
-						style={{
-							...styles.connectionText,
-							color: connectionStatus.failed ? "orange" : "white",
-							marginTop: 0,
-						}}
-					>
-						{connectionStatus.failed &&
-							"Also make sure, your mobile data is off."}
-					</Text>
-				</>
-			)}
-			{connectionStatus.connected && isStarted && channel === 2 && <Keypad />}
+						{channel === 2 && (
+							<TouchableOpacity
+								style={styles.outputWrapper}
+								onPress={() => handleOutputChange(2)}
+								activeOpacity={0.8}
+							>
+								<Text style={styles.output}>Output 2</Text>
+								<View
+									style={{
+										...styles.led,
+										backgroundColor:
+											output2Status === "on" ? "#46cf76" : "lightgray",
+									}}
+								/>
+							</TouchableOpacity>
+						)}
+					</View>
+				) : (
+					<>
+						<Text
+							style={{
+								...styles.connectionText,
+								color: connectionStatus.failed ? "orange" : "white",
+							}}
+						>
+							{connectionStatus.failed &&
+								"Please connect your wifi with WiFi Clip"}
+						</Text>
+						<Text
+							style={{
+								...styles.connectionText,
+								color: connectionStatus.failed ? "orange" : "white",
+								marginTop: 0,
+							}}
+						>
+							{connectionStatus.failed &&
+								"Also make sure, your mobile data is off."}
+						</Text>
+					</>
+				)}
+			</View>
+			<Button title={`Switch to sport mode`} color="#395278e3" />
+			{/* {connectionStatus.connected && isStarted && channel === 2 && <Keypad />} */}
 		</View>
 	);
 };
@@ -198,6 +203,9 @@ const Controller = ({ route }) => {
 export default Controller;
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
 	statusWrapper: {
 		marginBottom: 10,
 		flexDirection: "row",
